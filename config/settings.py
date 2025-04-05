0814bfb77c15aab6b5600d47acfa856d714107b3
aleksandr <kharitonov_am@bk.ru>
2025-04-05 11:06:22 +0300

    Подключил СУБД PostgreSQL для работы в проекте

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,16 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 import catalog.apps
 
+load_dotenv(override=True)
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-=9$&n$^&no7y18ns+j0pmno=9-j)k0ro&j1yn+iuyvt8scfrp^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = []
 
@@ -53,8 +57,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
