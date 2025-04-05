2bbdea8ab6a50d52a2e71d685d4e30456d6315c7
aleksandr <kharitonov_am@bk.ru>
2025-04-05 12:17:52 +0300

    Настроил админку логин и пароль суперпользователя admin/admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Product
+# log admin
+# pas admin
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category',)
+    search_fields = ('name', 'description',)
+    list_filter = ('category',)
+
