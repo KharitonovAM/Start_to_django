ab4be49a7ad018f63e44dc4ab33062e8344e7977
aleksandr <kharitonov_am@bk.ru>
2025-04-12 22:43:47 +0300

    Выполнил задание по странице для продукта и главной

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
 from .apps import CatalogConfig
-from .views import contact, home
+from .views import *
 
 app_name = CatalogConfig.name
 
-urlpatterns = [path("", home), path("contact/", contact)]
+urlpatterns = [
+    path("", list_products, name='index'),
+    path("contact/", contact, name='contact'),
+    path("base/", base_page, name='support'),
+    path('products/<int:pk>/', products, name='select_product')
+
+               ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
@@ -9,3 +10,16 @@ def home(request):
 
 def contact(request):
     return render(request, "contacts.html")
+
+def base_page(reguest):
+    return render(reguest, 'base.html')
+
+def products(request, pk):
+    product=Product.objects.get(pk=pk)
+    contex = {'product': product}
+    return render(request, 'products.html', context=contex)
+
+def list_products(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'home.html', context=context)
