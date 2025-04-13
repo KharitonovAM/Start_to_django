75ab8e2010ba6557558ac99e9ad352665f0ba701
aleksandr <kharitonov_am@bk.ru>
2025-04-13 10:31:06 +0300

    Решил проблему с отображением картинок

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from .apps import CatalogConfig
 from .views import *
@@ -6,9 +9,11 @@ from .views import *
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", list_products, name='index'),
-    path("contact/", contact, name='contact'),
-    path("base/", base_page, name='support'),
-    path('products/<int:pk>/', products, name='select_product')
+    path("", list_products, name="index"),
+    path("contact/", contact, name="contact"),
+    path("base/", base_page, name="support"),
+    path("products/<int:pk>/", products, name="select_product"),
+]
 
-               ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 from .models import Product
 
 # Create your views here.
@@ -11,15 +12,18 @@ def home(request):
 def contact(request):
     return render(request, "contacts.html")
 
+
 def base_page(reguest):
-    return render(reguest, 'base.html')
+    return render(reguest, "base.html")
+
 
 def products(request, pk):
-    product=Product.objects.get(pk=pk)
-    contex = {'product': product}
-    return render(request, 'products.html', context=contex)
+    product = Product.objects.get(pk=pk)
+    contex = {"product": product}
+    return render(request, "products.html", context=contex)
+
 
 def list_products(request):
     products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'home.html', context=context)
+    context = {"products": products}
+    return render(request, "home.html", context=context)
