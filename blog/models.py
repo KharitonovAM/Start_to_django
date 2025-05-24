bbecc53cb833b49d2524c0d5f1a35e9ddc9f11de
aleksandr <kharitonov_am@bk.ru>
2025-05-25 00:53:58 +0300

    Откорректировал отображение стартовой страницы блога

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
 class Publication(models.Model):
-
     title = models.CharField(
-        max_length=100, verbose_name='Заголовок', help_text='Укажите заголовок'
+        max_length=100,
+        verbose_name='Заголовок',
+        help_text='Укажите заголовок'
     )
     content = models.TextField(
-        verbose_name="Содержание", help_text="Введите содержание статьи"
+        verbose_name="Содержание",
+        help_text="Введите содержание статьи"
     )
     preview = models.ImageField(
         verbose_name='Preview',
@@ -17,19 +20,19 @@ class Publication(models.Model):
         blank=True
     )
     create_data = models.DateField(
-       verbose_name="дата создания",
-       help_text="Введите дату создания",
-       blank=True,
-       null=True
+        verbose_name="дата создания",
+        help_text="Введите дату создания",
+        blank=True,
+        null=True
     )
     is_publicated = models.BooleanField(
-       verbose_name= 'Признак пуликации',
-       help_text='Укажите, опубликована статья ии нет',
-       default=True
+        verbose_name='Признак пуликации',
+        help_text='Укажите, опубликована статья ии нет',
+        default=True
     )
     number_shows = models.IntegerField(
-    verbose_name="Количество просмотров",
-    default=0
+        verbose_name="Количество просмотров",
+        default=0
     )
 
     def __str__(self):
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from .views import BlogListView
 
 from .views import *
-urlpatterns = [path("blog/", TemplateView.as_view(template_name='blog/base.html'), name="blog"),
+urlpatterns = [path("blog/", BlogListView.as_view(), name="blog_list"),
+
                ]
