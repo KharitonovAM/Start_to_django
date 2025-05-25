18c125fd781d2532338e76d207e8988886a5ab84
aleksandr <kharitonov_am@bk.ru>
2025-05-25 11:15:05 +0300

    Добавил ффункционал для добавления новой статьи (какая-то проблема с отображением формы - не переходит по ссылке)

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from .views import BlogListView, BlogDetailView, BlogCreateView
 from .apps import BlogConfig
 
 app_name = BlogConfig.name
 
 urlpatterns = [path("blog/", BlogListView.as_view(), name="blog_list"),
                path("blog/<int:pk>", BlogDetailView.as_view(), name='blog_detail'),
+               path("blog/", BlogCreateView.as_view(), name='create_blog')
                ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Publication
 
 
@@ -8,3 +9,9 @@ class BlogListView(ListView):
 
 class BlogDetailView(DetailView):
     model = Publication
+
+
+class BlogCreateView(CreateView):
+    model = Publication
+    fields = ('title', 'content', 'preview', 'create_data', 'is_publicated', 'number_shows')
+    success_url = reverse_lazy('blog:blog_list')
