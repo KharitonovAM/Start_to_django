fefe1266cfe452ee5b89dc9a2ce67244f9ab4fbe
aleksandr <kharitonov_am@bk.ru>
2025-05-30 22:43:28 +0300

    Выполнил задачу 2

--- catalog/forms.py
+++ catalog/forms.py
@@ -5,6 +5,7 @@ from .models import Product, Category
 
 spam_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 
+
 class ProductForm(ModelForm):
     class Meta:
         model = Product
@@ -14,9 +15,12 @@ class ProductForm(ModelForm):
     def clean(self):
         name = self.cleaned_data['name']
         description = self.cleaned_data['description']
-        if set(name.lower().split())&set(spam_words) or set(description.lower().split())&set(spam_words):
-            message = f'Спам-слова запрещено использовать, необходимо убрать:\n {' '.join(set(name.lower().split())&set(spam_words))} {' '.join(set(description.lower().split())&set(spam_words))}!!!'
+        if set(name.lower().split()) & set(spam_words) or set(description.lower().split()) & set(spam_words):
+            message = f'Спам-слова запрещено использовать, необходимо убрать:\n {' '.join(set(name.lower().split()) & set(spam_words))} {' '.join(set(description.lower().split()) & set(spam_words))}!!!'
             raise ValidationError(message)
 
-
-
+    def clean_price(self):
+        price = self.cleaned_data['price']
+        if int(price) < 0:
+            raise ValidationError("Недопустима отрицательная цена!")
+        return price
