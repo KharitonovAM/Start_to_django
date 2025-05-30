caab4c257dee0911032b8ba9332d8b96c3fbe879
aleksandr <kharitonov_am@bk.ru>
2025-05-30 23:55:47 +0300

    добавил обработку чекбокса

--- catalog/forms.py
+++ catalog/forms.py
@@ -10,6 +10,8 @@ class StyleFormMixim:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for fild_name, fild in self.fields.items():
+            if isinstance(fild, BooleanField):
+                fild.widget.attrs['class'] = "form-check-input"
             fild.widget.attrs['class'] = "form-control"
 
 class ProductForm(StyleFormMixim, ModelForm):
