0a88d76764b4d28f60b589717b1c785726074de0
aleksandr <kharitonov_am@bk.ru>
2025-07-29 22:55:15 +0300

    исправил пару ошибок оформления

--- users/views.py
+++ users/views.py
@@ -1,28 +1,19 @@
-import os
-import secrets
-
-from django.contrib.auth import login
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
+from config.settings import EMAIL_HOST, EMAIL_HOST_USER
 from users.forms import UserRegisterForm
 from users.models import User
-
-from config.settings import EMAIL_HOST, EMAIL_PORT, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_USE_TLS, EMAIL_USE_SSL
 from .forms import UserForm
-class UserCreateView(CreateView):
 
+
+class UserCreateView(CreateView):
 
     model = User
     form_class = UserRegisterForm
     success_url = reverse_lazy('users:login')
-
 
     def form_valid(self, form):
         object = form.save()
